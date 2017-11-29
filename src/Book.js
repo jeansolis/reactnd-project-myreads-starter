a90476310964572fea5e843929f5efc8e159b059
Jean J. Solís <jeansolisa@gmail.com>
2017-11-29 17:21:59 -0600

    fix: Rewrite Book component as a Stateless Functional Component.

--- src/Book.js
+++ src/Book.js
@@ -1,29 +1,21 @@
-import React, {Component} from 'react'
+import React from 'react'
 import BookshelfChanger from './BookshelfChanger'
-import PropTypes from 'prop-types'
 
-class Book extends Component {
-    static propTypes = {
-        book: PropTypes.object.isRequired,
-        onShelfChange: PropTypes.func.isRequired
-    }
-
-    render() {
-        const {book} = this.props
-
-        return (
-            <div className="book">
-                <div className="book-top">
+function Book(props){
+    const {book} = props
+    
+    return (
+        <div className="book">
+            <div className="book-top">
                 <div className="book-cover" style={{ width: 128, height: 192, 
                 backgroundImage: 'url("' + ((book.imageLinks && book.imageLinks.smallThumbnail) ? book.imageLinks.smallThumbnail : '') + '")' }} >
                 </div>
-                    <BookshelfChanger book={book} onShelfChange={this.props.onShelfChange} />
-                </div>
-                <div className="book-title">{book.title}</div>
-                 <div className="book-authors">{book.authors}</div>
+                <BookshelfChanger book={book} onShelfChange={props.onShelfChange} />
             </div>
-        )
-    }
+            <div className="book-title">{book.title}</div>
+            <div className="book-authors">{book.authors}</div>
+        </div>
+    )
 }
 
 export default Book
